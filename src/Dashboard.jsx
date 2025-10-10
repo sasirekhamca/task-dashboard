d57d9416040dddadde6259f8f55d44d6643daa41
sasirekha <sasirekhamca@gmail.com>
2025-10-10 18:14:17 +0530

    Day 1: Add Dashboard component and basic styling

---
+++ src/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Dashboard.css";
+function Dashboard() {
+  return (
+    <div>
+      <h1>Task-Dashboard</h1>
+      <p>Welcome to task manager</p>
+    </div>
+  );
+}
+export default Dashboard;
