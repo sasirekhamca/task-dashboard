c2eb011bb3b20938a761a3b1bdca288399188e88
sasirekha <sasirekhamca@gmail.com>
2025-10-13 12:30:51 +0530

    Day 3: Add TaskItem component with props

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Dashboard.css";
 import Header from "./Header";
+import TaskItem from "./TaskItem";
 function Dashboard() {
   return (
     <div>
       <Header title="Task-Dashboard"></Header>
       <p>Welcome to task manager</p>
+      <TaskItem title="Learn React" completed={false} />
+      <TaskItem title="Build App" completed={true} />
+      <TaskItem />
     </div>
   );
 }
